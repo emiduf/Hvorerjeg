d336ea9e87cefeb3ff5a79499c3e911ee6a39d71
Sunniva Sørensen <sunnsor@gmail.com>
2025-08-29 12:28:24 +0200

    Update tts.js
    Lagt til tekst til tale (test)

---
+++ client/src/lib/tts.js
@@ -0,0 +1,24 @@
+// leser opp tekst hvis nettleseren støtter det
+export function tale(tekst) {
+  if (!('speechSynthesis' in window)) return;
+
+// Stopp eventuell pågående tale
+window.speechSynthesis.cancel();
+
+// Konfigurer tale
+const utter = new SpeechSynthesisUtterance(tekst);
+utter.lang = 'nb-NO';
+utter.rate = 1;
+utter.pitch = 1;
+
+// Velg norsk stemme hvis tilgjengelig
+const voices = window.speechSynthesis.getVoices();
+const norskStemme = voices.find(voice => voice.lang === 'nb-NO');
+if (norskStemme) {
+  utter.voice = norskStemme;
+}
+
+  // Start tale
+  window.speechSynthesis.tale(utter);
+
+}
